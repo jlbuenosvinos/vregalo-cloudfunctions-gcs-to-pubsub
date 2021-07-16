fa0b3173cb4f1b4dac7b93eab18a00d6384cef4c
Juan Luis Buenosvinos <jbuenosv@google.com>
2021-07-16 20:11:11 +0200

    Initial commit

---
+++ src/main/java/com/google/jbuenosv/inditex/poc/vregalo/cloudfunctions/googlecloudfunctions/FromGcsToPubSubCloudFunction.java
@@ -0,0 +1,34 @@
+package com.google.jbuenosv.inditex.poc.vregalo.cloudfunctions.googlecloudfunctions;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import com.google.cloud.functions.BackgroundFunction;
+import com.google.cloud.functions.Context;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+/**
+ * Created by jbuenosv@google.com
+ */
+@ApplicationScoped
+public class FromGcsToPubSubCloudFunction implements BackgroundFunction<StorageEvent> {
+
+    public static final Logger logger = LoggerFactory.getLogger(FromGcsToPubSubCloudFunction.class);
+
+    @Counted(name = "countAccept", description = "Counts how many times the processOrder method has been invoked")
+    @Timed(name = "timeAccept", description = "Times how long it takes to invoke the accept method", unit = MetricUnits.MILLISECONDS)
+    @Override
+    public void accept(StorageEvent event, Context context) throws Exception {
+        logger.debug("Receive event on ",event.toString());
+
+
+
+        logger.debug("End receive event on ",event.getName());
+    }
+
+}
---
+++ src/main/java/com/google/jbuenosv/inditex/poc/vregalo/cloudfunctions/googlecloudfunctions/StorageEvent.java
@@ -0,0 +1,48 @@
+package com.google.jbuenosv.inditex.poc.vregalo.cloudfunctions.googlecloudfunctions;
+
+/**
+ * Created by jbuenosv@google.com
+ */
+public class StorageEvent {
+
+    private String bucket;
+    private String name;
+    private String metageneration;
+
+    public StorageEvent() {
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMetageneration() {
+        return metageneration;
+    }
+
+    public void setMetageneration(String metageneration) {
+        this.metageneration = metageneration;
+    }
+
+    @Override
+    public String toString() {
+        return "StorageEvent {" +
+                "bucket='" + bucket + '\'' +
+                ", name='" + name + '\'' +
+                ", metageneration='" + metageneration + '\'' +
+                '}';
+    }
+
+}
